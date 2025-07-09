553d9a4c04360cdc5be65069eceddd2ba3c4a53d
dariangaribotti <darian.gb03@gmail.com>
2025-07-09 17:48:01 -0300

    cambios

---
+++ numeros_pares.php
@@ -0,0 +1,23 @@
+<?php 
+
+for($i = 2; $i <= 100; $i+=2){
+    echo $i . "<br>";
+    
+    if ($i == 60){
+        break;
+    } 
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
